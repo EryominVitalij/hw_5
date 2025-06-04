36bda971782c53cec0bb6a7184c0eefcd56f5e4b
Vitalij Eryomin <Eryomin@bk.ru>
2025-06-04 15:47:41 +0300

    добавил conftest.py

--- test/test_fill_registration_demoqa.py
+++ test/test_fill_registration_demoqa.py
@@ -3,15 +3,6 @@
 from selene import browser
 import os.path
 
-def date_generator(day: int, month: int, year: int):
-    browser.element('#dateOfBirthInput').click()
-    browser.element(f'.react-datepicker__month-select option[value="{month - 1}"]').click()
-    browser.element(f'.react-datepicker__year-select option[value="{year}"]').click()
-    if day >= 10:
-        browser.element(f'.react-datepicker__day.react-datepicker__day--0{day}').click()
-    else:
-        browser.element(f'.react-datepicker__day.react-datepicker__day--00{day}').click()
-
 def test_fill_registration_demoqa(window_browser):
     browser.open('https://demoqa.com/automation-practice-form') # Открытие страницы для заполнения формы
 
@@ -25,6 +16,15 @@ def test_fill_registration_demoqa(window_browser):
     browser.element('#userNumber').type('9109176995') # Контактный номер
 
     # Выбор даты рождения
+    def date_generator(day: int, month: int, year: int):
+        browser.element('#dateOfBirthInput').click()
+        browser.element(f'.react-datepicker__month-select option[value="{month - 1}"]').click()
+        browser.element(f'.react-datepicker__year-select option[value="{year}"]').click()
+        if day >= 10:
+            browser.element(f'.react-datepicker__day.react-datepicker__day--0{day}').click()
+        else:
+            browser.element(f'.react-datepicker__day.react-datepicker__day--00{day}').click()
+
     browser.element('#dateOfBirthInput').click()
     date_generator(22, 1, 1978)
 
