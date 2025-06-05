58de5db4e2ba11786812eb0b0af696ab2d750217
Vitalij Eryomin <Eryomin@bk.ru>
2025-06-05 16:10:57 +0300

    внёс правки в код по рекомендациям наставника (пернёс функцию перед тестом и добавил проверку)

--- test/test_fill_registration_demoqa.py
+++ test/test_fill_registration_demoqa.py
@@ -1,7 +1,17 @@
 
 
-from selene import browser
-import os.path
+from selene import browser, have
+import os
+
+# Генератор даты
+def date_generator(day: int, month: int, year: int):
+    browser.element('#dateOfBirthInput').click()
+    browser.element(f'.react-datepicker__month-select option[value="{month - 1}"]').click()
+    browser.element(f'.react-datepicker__year-select option[value="{year}"]').click()
+    if day >= 10:
+        browser.element(f'.react-datepicker__day.react-datepicker__day--0{day}').click()
+    else:
+        browser.element(f'.react-datepicker__day.react-datepicker__day--00{day}').click()
 
 def test_fill_registration_demoqa(window_browser):
     browser.open('https://demoqa.com/automation-practice-form') # Открытие страницы для заполнения формы
@@ -9,23 +19,13 @@ def test_fill_registration_demoqa(window_browser):
     # Заполнение регистрационной формы
 
     # Персональные данные
-    browser.element('#firstName').type('Виталий')  # Имя
-    browser.element('#lastName').type('Ерёмин') # Фамилия
+    browser.element('#firstName').type('Vitalij')  # Имя
+    browser.element('#lastName').type('Eryomin') # Фамилия
     browser.element('#userEmail').type('Eryomin@bk.ru') # Адрес электронной почты
     browser.element('[for="gender-radio-1"]').click() # Пол
     browser.element('#userNumber').type('9109176995') # Контактный номер
 
     # Выбор даты рождения
-
-    # Генератор даты
-    def date_generator(day: int, month: int, year: int):
-        browser.element('#dateOfBirthInput').click()
-        browser.element(f'.react-datepicker__month-select option[value="{month - 1}"]').click()
-        browser.element(f'.react-datepicker__year-select option[value="{year}"]').click()
-        if day >= 10:
-            browser.element(f'.react-datepicker__day.react-datepicker__day--0{day}').click()
-        else:
-            browser.element(f'.react-datepicker__day.react-datepicker__day--00{day}').click()
 
     browser.element('#dateOfBirthInput').click()
     date_generator(22, 1, 1978)
@@ -45,4 +45,20 @@ def test_fill_registration_demoqa(window_browser):
 
     # Подтверждение отправки заполненной формы
     browser.element('#submit').click()
+
+    # Проверка
+    assert browser.element('.table-responsive').all('td').should(
+        have.exact_texts(
+            'Student Name', 'Vitalij Eryomin',
+            'Student Email', 'Eryomin@bk.ru',
+            'Gender', 'Male',
+            'Mobile', '9109176995',
+            'Date of Birth', '22 January,1978',
+            'Subjects', '', 'Hobbies', 'Sports, Reading',
+            'Picture', 'img.png',
+            'Address', 'Obninsk, Blohinceva street, 11',
+            'State and City', 'Haryana Karnal'
+        )
+    )
+
 
